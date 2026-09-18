37bc0ac1c9f26dae15b595c00e60e99d473400e6
lazycat-elyn <hongcaerra@gmail.com>
2026-09-18 11:35:38 +0800

    Load Kitchen 3.1 reference fix

--- kitchen-3d-bootstrap-21.js
+++ kitchen-3d-bootstrap-21.js
@@ -20,6 +20,7 @@ try{
   await import('./kitchen-3d-render-match-29-hotfix.js?v=2910');
   await import('./kitchen-3d-render-match-29-fidelity.js?v=2910');
   await import('./kitchen-3d-reference-match-30.js?v=3000');
+  await import('./kitchen-3d-reference-match-31-fix.js?v=3100');
 }finally{
   URL.revokeObjectURL(blobUrl);
 }
